2f095e7b9c1bccfe68294a8c4d712eb5487ab550
ar2747511-lang <ar2747511@gmail.com>
2026-03-05 13:33:46 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDfFH03zJIwuh3n3M3DdwhKAjBV65J91JA",
   authDomain: "happychild-orders-525f7.firebaseapp.com",
   projectId: "happychild-orders-525f7",
-  storageBucket: "happychild-orders-525f7.firebasestorage.app",
+  storageBucket: "happychild-orders-525f7.appspot.com",
   messagingSenderId: "448720716450",
   appId: "1:448720716450:web:f5648d2ce34e02f1f98dfb",
   measurementId: "G-DF6CLPB133"
