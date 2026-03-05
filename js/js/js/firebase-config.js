9bc97515ea77729b6986e871fe2308f6ba6a1d62
ar2747511-lang <ar2747511@gmail.com>
2026-03-05 02:10:58 +0100

    Update firebase-config.js

--- js/js/js/firebase-config.js
+++ js/js/js/firebase-config.js
@@ -1,1 +1,11 @@
-
+// 1) Create a Firebase project: https://console.firebase.google.com
+// 2) Add a Web App and copy the config object below.
+// 3) Paste it between the braces.
+export const firebaseConfig = {
+ apiKey: "AIzaSyCEITsi736SdHkZKpcaUVCvXgp5-Fd-s2E",
+  authDomain: "happychild-orders.firebaseapp.com",
+  projectId: "happychild-orders",
+  storageBucket: "happychild-orders.firebasestorage.app",
+  messagingSenderId: "902705815946",
+  appId: "1:902705815946:web:933764455a820464046873",
+};
